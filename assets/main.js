1c8708aba65fcfa283e300b402ba0fc5f6939113
Ken Nguyen <kennguyen@KennoMacBook-Air-2.local>
2025-03-26 21:52:27 +0900

    add analytics

--- assets/main.js
+++ assets/main.js
@@ -1,7 +1,9 @@
 import Alpine from 'alpinejs'
 import { injectSpeedInsights } from '@vercel/speed-insights';
+import { inject } from "@vercel/analytics"
 
 injectSpeedInsights();
+inject();
 
 window.Alpine = Alpine
 
