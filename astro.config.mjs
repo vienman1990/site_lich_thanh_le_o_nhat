7cce35b8d57e4086c09da7766bc2bfc9c980c8b7
Ken Nguyen <kennguyen@KennoMacBook-Air-2.local>
2025-04-06 22:52:38 +0900

    [convert to astro]

---
+++ astro.config.mjs
@@ -0,0 +1,15 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import tailwindcss from '@tailwindcss/vite';
+
+import sitemap from '@astrojs/sitemap';
+
+// https://astro.build/config
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()]
+  },
+  site: 'https://lich-thanh-le-o-nhat-ban.vercel.app',
+  integrations: [sitemap()]
+});
